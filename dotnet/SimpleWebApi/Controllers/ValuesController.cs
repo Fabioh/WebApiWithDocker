1d72e496bf0dd31e146314794aa92e4d9e2788f6
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2017-12-18 00:52:30 -0200

    -Initial commit - Simple WebApi created - DockerFile created and working

---
+++ dotnet/SimpleWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+	[Produces("application/json")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string value)
+        {
+            return Ok(value);
+        }
+    }
+}
